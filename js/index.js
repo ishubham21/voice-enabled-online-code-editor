681fd08ff2e777ef0c4ee38e219afb2105537766
Shubham Gautam <sg2199203@gmail.com>
2021-02-10 17:49:21 +0530

    Reducing the complexity of the code. Removed loop

--- js/index.js
+++ js/index.js
@@ -5,15 +5,14 @@
     //selecting all the buttons
     const ul = document.querySelector('ul')
 
-    ul.addEventListener('click', (evt) => {
-
-        const selectedBlk = e.taget
-        var blkId = selectedBlk.getAttribute('data-target')
+    ul.addEventListener('click', (e) => {
+        var blkId = e.target.getAttribute('data-target')
         
         var blkToEnable = document.querySelector(blkId)
 
         blkToEnable.classList.toggle('selected')
     })
+    const btns = document.querySelectorAll('.control-btn')
 
     //iterating over buttons
     // for (let i = 0; i < btns.length; i++) {
