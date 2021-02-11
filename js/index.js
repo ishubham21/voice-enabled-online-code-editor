75a868d7723e1f29033e9fd4170cb01eda3ae3e5
Shubham Gautam <sg2199203@gmail.com>
2021-02-11 20:24:10 +0530

    Commenting up the code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,70 @@
+const getBlobURL = (code, type) => {
+    
+    //creating a blob (Binarly Large Object)
+    const blob = new Blob([code], { type })
+    //creating  a URL for blob
+    return URL.createObjectURL(blob)
+}
+
+const getGeneratedUrl = ({ html, css, js }) => {
+    
+    //getting URL for css and js
+    const cssURL = getBlobURL(css, 'text/css')
+    const jsURL = getBlobURL(js, 'text/javascript')
+
+    //source - HTML
+    const sourceHTML = `
+    <html>
+      <head>
+        ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
+        ${js && `<script src="${jsURL}"></script>`}
+      </head>
+      <body>
+        ${html || ''}
+      </body>
+    </html>
+  `
+    //returning a link for HTML code with links
+    return getBlobURL(sourceHTML, 'text/html')
+}
+
+const getCodeValues = () => {
+
+    //getting values of textboxes for compilation
+    const html = document.querySelector('#html').value
+    const css = document.querySelector('#css').value
+    const js = document.querySelector('#js').value
+
+    if (!html) {
+        alert('Cannot run empty html')
+        return null
+    }
+
+    //codes object containing codes from all the blocks
+    const codes = { html, css, js }
+
+
+    return codes
+}
+
+//click event listener for run button
+var run = document.querySelector('#run')
+run.addEventListener('click', () => {
+
+    const codeValues = getCodeValues()
+    
+    if (codeValues != null) {
+
+        const urlForIframe = getGeneratedUrl(codeValues)        
+        const iframe = document.querySelector('#result')
+        
+        //giving url to iframe - setting attribute
+        iframe.setAttribute('src', urlForIframe)
+    
+    }
+
+})
+
 //Immediately Invoked Function Expression
 (function () {
 
@@ -18,79 +85,3 @@
     })
     
 })();
-
-// $('#run').on('click', function () {
-//     var str = '<html><body>~ht</body><style>~cs</style><script>~js</ script></html>';
-
-//     str = str.replace('~ht', $('#html').val());
-//     str = str.replace('~cs', $('#css').val());
-//     str = str.replace('~js', $('#js').val());
-
-//     $('iframe').attr('srcdoc', str);
-// });
-
-const getBlobURL = (code, type) => {
-    const blob = new Blob([code], { type })
-    return URL.createObjectURL(blob)
-}
-const getGeneratedUrl = ({ html, css, js }) => {
-    
-    const cssURL = getBlobURL(css, 'text/css')
-    const jsURL = getBlobURL(js, 'text/javascript')
-
-    const source = `
-    <html>
-      <head>
-        ${css && `<link rel="stylesheet" type="text/css" href="${cssURL}" />`}
-        ${js && `<script src="${jsURL}"></script>`}
-      </head>
-      <body>
-        ${html || ''}
-      </body>
-    </html>
-  `
-
-    return getBlobURL(source, 'text/html')
-}
-
-
-// const url = getGeneratedPageURL({
-//     html: htm,
-//     css: cs,
-//     js: jss
-// })
-
-// const iframe = document.querySelector('iframe')
-// iframe.src = url
-
-function getCodeValues(){
-
-    //getting values of textboxes for compilation
-    const html = document.querySelector('#html').value
-    const css = document.querySelector('#css').value
-    const js = document.querySelector('#js').value
-
-    if (!html) {
-        alert('Cannot run empty html')
-        return null
-    }
-
-    //codes object containing codes from all the blocks
-    const codes = { html, css, js }
-    return codes
-}
-
-var run = document.querySelector('#run')
-run.addEventListener('click', () => {
-
-    const codeValues = getCodeValues()
-    
-    if (codeValues != null) {
-        const urlForIframe = getGeneratedUrl(codeValues)
-        
-        const iframe = document.querySelector('#result')
-        iframe.src = urlForIframe
-    }
-
-})
-
